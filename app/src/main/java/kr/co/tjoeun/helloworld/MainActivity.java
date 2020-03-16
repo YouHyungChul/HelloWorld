684a2480df63cc30f262814677546636d265cb08
YouHyungChul <61854958+YouHyungChul@users.noreply.github.com>
2020-03-16 13:03:52 +0900

    주석만 달아봄.

--- app/src/main/java/kr/co/tjoeun/helloworld/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/helloworld/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//    첫 프로젝트 주석
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
